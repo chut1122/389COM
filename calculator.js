e56fc6c7b55560434a9503771fba41e3f35aeefb
chut1122 <chut_95@hotmail.com>
2019-07-06 16:02:26 +0800

    Add files via upload
    update

--- calculator.js
+++ calculator.js
@@ -1,185 +1,404 @@
-var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '/', '%', 'Xy'];
-var decimalAdded = false;
-
-
-function calculate(select) {
-
-    var symbol = {
-        sum: '+',
-        minus: '-',
-        div: '/',
-        mul: '*',
-        mod: '%',
-        power: '^'
-    };
-
-    symbol.ooo = [[[symbol.sum], [symbol.minus], [symbol.div], [symbol.mul]], [[symbol.mod], [symbol.power]]];
-
-    select = select.replace(/[^0-9%^*\/()\-+.]/g, '');
-
-    var output;
-    for (var i = 0, n = symbol.ooo.length; i < n; i++) {
-
-        var re = new RegExp('(\\d+\\.?\\d*)([\\' + symbol.ooo[i].join('\\') + '])(\\d+\\.?\\d*)');
-        re.lastIndex = 0;
-
-        while (re.test(select)) {
-            output = calc_internal(RegExp.$1, RegExp.$2, RegExp.$3);
-            if (isNaN(output) || !isFinite(output)) return output;
-            select = select.replace(re, output);
+$(document).ready(function() {
+  
+  // A database of the symbols and functions of every operand. Order of operators determines precedence.
+  var operators = [
+    {
+      id: "op-power",
+      numOperands: 2,
+      symbol: " ^ ",
+      calc: function(a, b) {
+        return Math.pow(a, b);
+      }
+    },
+    {
+      id: "op-negate",
+      numOperands: 1,
+      symbol: " -",
+      calc: function(a) {
+        return -a;
+      }
+    },
+    {
+      id: "op-square-root",
+      numOperands: 1,
+      symbol: " √",
+      calc: function(a) {
+        return Math.sqrt(a);
+      }
+    },
+    {
+      id: "op-log",
+      numOperands: 1,
+      symbol: " log ",
+      calc: function(a) {
+        return Math.log10(a);
+      }
+    },
+    {
+      id: "op-natural-log",
+      numOperands: 1,
+      symbol: " ln ",
+      calc: function(a) {
+        return Math.log(a);
+      }
+    },
+    {
+      id: "op-sin",
+      numOperands: 1,
+      symbol: " sin ",
+      calc: function(a) {
+        return Math.sin(a);
+      }
+    },
+    {
+      id: "op-cos",
+      numOperands: 1,
+      symbol: " cos ",
+      calc: function(a) {
+        return Math.cos(a);
+      }
+    },
+    {
+      id: "op-tan",
+      numOperands: 1,
+      symbol: " tan ",
+      calc: function(a) {
+        return Math.tan(a);
+      }
+    },
+    {
+      id: "op-inverse-sin",
+      numOperands: 1,
+      symbol: " sin-1 ",
+      calc: function(a) {
+        return Math.asin(a);
+      }
+    },
+    {
+      id: "op-inverse-cos",
+      numOperands: 1,
+      symbol: " cos-1 ",
+      calc: function(a) {
+        return Math.acos(a);
+      }
+    },
+    {
+      id: "op-inverse-tan",
+      numOperands: 1,
+      symbol: " tan-1 ",
+      calc: function(a) {
+        return Math.atan(a);
+      }
+    },
+    {
+      id: "op-e",
+      numOperands: 1,
+      symbol: " e ^ ",
+      calc: function(a) {
+        return Math.exp(a);
+      }
+    },
+    {
+      id: "op-nth-root",
+      numOperands: 2,
+      symbol: "*√",
+      calc: function(a, b) {
+        return Math.pow(b, 1/a);
+      }
+    },
+    {
+      id: "op-multiply",
+      numOperands: 2,
+      symbol: " x ",
+      calc: function(a, b) {
+        return a * b;
+      }
+    },
+    {
+      id: "op-divide",
+      numOperands: 2,
+      symbol: " ÷ ",
+      calc: function(a, b) {
+        return a / b;
+      }
+    },
+    {
+      id: "op-add",
+      numOperands: 2,
+      symbol: " + ",
+      calc: function(a, b) {
+        return a + b;
+      }
+    },
+    {
+      id: "op-subtract",
+      numOperands: 2,
+      symbol: " - ",
+      calc: function(a, b) {
+        return a - b;
+      }
+    }
+  ];
+  
+  // The number of places to round to
+  const roundPlaces = 15;
+  
+  // Get the operator object for a given operator ID
+  function getOperator(opID) {
+    for(var i = 0; i < operators.length; i++) {
+      if(operators[i].id === opID) {
+        return operators[i];
+      }
+    }
+    return undefined;
+  }
+  
+  // Get the precedence of an operator given its ID
+  function getOpPrecedence(opID) {
+    for(var i = 0; i < operators.length; i++) {
+      if(operators[i].id === opID) {
+        return i;
+      }
+    }
+    
+    // If the given ID does not return an operator, then return a large value that will always lose in precedence
+    return 1000;
+  }
+      
+  // Returns true if op1 ID has equal or higher precedence than op2 ID, false otherwise
+  function hasPrecedence(op1, op2) {
+    if(getOperator(op1) != undefined) {
+      return getOpPrecedence(op1) <= getOpPrecedence(op2);
+    }
+  }
+  
+  // Converts the given radian value to degrees
+  function toDegrees(radians) {
+    return radians * (180 / Math.PI);
+  }
+  
+  // Converts the given degrees value to radians
+  function toRadians(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+  
+  // A list of every token (number or operator) currently in the expression
+  var tokenList = [];
+  
+  // A list of previous results and expressions in the form {out: output, expression: expression string, tokens: list of tokens in the expression}
+  var calcHistory = [];
+  
+  // Evaluates the expression and outputs the result
+  function calculate() {
+    // Check if brackets are balanced
+    var count = 0;
+    for(var i = 0; i < tokenList.length; i++) {
+      if(tokenList[i] === "bracket-left") {
+        count++;
+      } else if(tokenList[i] === "bracket-right") {
+        count--;
+      }
+    }
+    if(count != 0) {
+      output("Error: unbalanced brackets");
+      return;
+    }
+    
+    // Evaluate the expression using a modified version of the shunting yard algorithm
+    var valStack = [];
+    var opStack = [];
+    
+    for(var i = 0; i < tokenList.length; i++) {
+      if(!isNaN(tokenList[i])) {
+        valStack.push(tokenList[i]);
+      } else if(tokenList[i] === "num-pi") {
+        valStack.push(Math.PI);
+      } else if(tokenList[i] === "bracket-left") {
+        opStack.push(tokenList[i]);
+      } else if(tokenList[i] === "bracket-right") {
+        while(opStack[opStack.length - 1] !== "bracket-left") {
+          var operator = getOperator(opStack.pop());
+          if(operator.numOperands === 1)
+            valStack.push(applyOperator(operator, [valStack.pop()]));
+          else
+            valStack.push(applyOperator(operator, [valStack.pop(), valStack.pop()]));
         }
+        opStack.pop();
+      } else {
+        while(opStack.length > 0 && hasPrecedence(opStack[opStack.length - 1], tokenList[i])) {
+          var operator = getOperator(opStack.pop());
+          if(operator.numOperands === 1)
+            valStack.push(applyOperator(operator, [valStack.pop()]));
+          else
+            valStack.push(applyOperator(operator, [valStack.pop(), valStack.pop()]));
+        }
+        opStack.push(tokenList[i]);
+      }
+    }
+    
+    while(opStack.length > 0) {
+      var operator = getOperator(opStack.pop());
+      if(operator.numOperands === 1)
+        valStack.push(applyOperator(operator, [valStack.pop()]));
+      else
+        valStack.push(applyOperator(operator, [valStack.pop(), valStack.pop()]));
+    }
+    
+    // Output the calculated result and the original expression
+    output(valStack[0], $("#expression").html(), tokenList);
+  }
+  
+  // Returns the result of applying the given unary or binary operator on the top values of the value stack
+  function applyOperator(operator, vals) {
+    var valA = vals[0];
+    var result;
+    
+    if(vals.length === 1) {
+      result = operator.calc(parseFloat(valA));
+    } else {
+      var valB = vals[1];
+      result = operator.calc(parseFloat(valB), parseFloat(valA));
     }
 
-    return output;
+    return result;
+  }
+  
+  // Updates the equation and calc history with the given output
+  function output(out, expression, tokens) {
+    out = +out.toFixed(roundPlaces);
+    $("#expression").html(out.toString());
+    
+    calcHistory.push({out: out, expression: expression, tokens: tokens});
+    $("#calc-history-box").html("");
+    for(var i = calcHistory.length - 1; i >= 0; i--) {
+      $("#calc-history-box").append("<p style='color: #B0B0B0; ' class='calc-history-eq' id='eq" + i + "'>" + calcHistory[i].expression + "</p><p style='text-align: right; margin-top: -10px;'>= " + calcHistory[i].out + "</p>");
+    }
+  }
+  
+  // Adds a token to the token list and updates the display
+  function addToken(token) {
+    if(isNaN(token)) {
+      if((token === "bracket-left" || token === "num-pi") && !isNaN(tokenList[tokenList.length - 1])) {
+        tokenList.push("op-multiply");
+      }
+      tokenList.push(token);
+    } else {
+      if(!isNaN(tokenList[tokenList.length - 1])) {
+        tokenList[tokenList.length - 1] = tokenList[tokenList.length - 1] + token;
+      } else {
+        if(!isNaN(token) && (tokenList[tokenList.length - 1] === "bracket-right" || tokenList[tokenList.length - 1] === "num-pi")) {
+          tokenList.push("op-multiply");
+        }
+        tokenList.push(token);
+      }
+    }
+        
+    displayEquation();
+  }
 
-    function calc_internal(a, op, b) {
-        a = a * 1; b = b * 1;
-
-        if (op == symbol.sum) {
-            return a + b;
+  // Updates the expression display's HTML
+  function displayEquation() {
+    var htmlString = "";
+    for(var i = 0; i < tokenList.length; i++) {
+      if(isNaN(tokenList[i])) {
+        if(tokenList[i] === "bracket-left") {
+          htmlString += " (";
+        } else if(tokenList[i] === "bracket-right") {
+          htmlString += ") ";
+        } else if(tokenList[i] === "num-pi") {
+          htmlString += " π ";
+        } else {
+          htmlString += getOperator(tokenList[i]).symbol;
         }
-        else if (op == symbol.minus) {
-            return a - b;
+      } else {
+        htmlString += tokenList[i];
+      }
+    }
+    $("#expression").html(htmlString);
+  }
+  
+  // Deletes the last entered token
+  function deleteLast() {
+    if(isNaN(tokenList[tokenList.length - 1])) {
+      tokenList.pop();
+    } else {
+      tokenList[tokenList.length - 1] = tokenList[tokenList.length - 1].slice(0, -1);
+      if(tokenList[tokenList.length -1].length === 0) {
+        tokenList.pop();
+      }
+    }
+    
+    displayEquation();
+  }
+  
+  // Shows/hides the advanced operators panel
+  function toggleAdvanced() {
+    $("#advanced-buttons").toggle();
+    if($("#advanced-buttons").is(":visible")) {
+      $("#toggle-advanced").removeClass("button-off");
+      $("#toggle-advanced span").removeClass("glyphicon-triangle-bottom").addClass("glyphicon-triangle-top");
+    } else {
+      $("#toggle-advanced").addClass("button-off");
+      $("#toggle-advanced span").removeClass("glyphicon-triangle-top").addClass("glyphicon-triangle-bottom");
+    }
+  }
+  
+  // Triggers the appropriate action for each button that can be pressed
+  function processButton(button) {
+    switch($(button).attr("id")) {
+      case "delete":
+        deleteLast();
+        break;
+      case "clear":
+        if(tokenList.length === 0) {
+          calcHistory.length = 0;
+          $("#calc-history-box").html("");
+        } else {
+          tokenList.length = 0;
+          displayEquation();
         }
-        else if (op == symbol.div) {
-            return a / b;
+        break;
+      case "period":
+        if(isNaN(tokenList[tokenList.length - 1])) {
+          addToken("0.");
+        } else {
+          if(tokenList[tokenList.length - 1].indexOf(".") === -1) {
+            tokenList[tokenList.length - 1] += ".";
+          }
         }
-        else if (op == symbol.mul) {
-            return a * b;
-        }
-        else if (op == symbol.mod) {
-            return a % b
-        }
-        else if (op == symbol.power) {
-            return Math.pow(a, b);
-        }
-        else {
-            return null;
+        displayEquation();
+        break;
+      case "equals":
+        calculate();
+        break;
+      case "toggle-advanced":
+        toggleAdvanced();
+        break;
+      case "num-pi":
+        addToken("num-pi");
+        break;
+      default:
+        if($(button).hasClass("num")) {
+          addToken($(button).html());
+        } else {
+          addToken($(button).attr("id"));
         }
     }
-}
-
-for (var i = 0; i < keys.length; i++) {
-    keys[i].onclick = function (e) {
-        var input = document.querySelector('.screen');
-        var inputVal = input.textContent;
-        var button = this.textContent;
-
-        if (button == 'copy') {
-            var copy = function (nmbr, mimetype) {
-                document.oncopy = function (event) {
-                    event.clipboardData.setData(mimetype, nmbr);
-                    event.preventDefault();
-                };
-                document.execCommand('Copy', false, null);
-            }
-            copy(inputVal, 'text/plain');
-            button = '';
-        }
-        if (button == 'c') {
-            input.textContent = '';
-            decimalAdded = false;
-        }
-
-        else if (button == '=') {
-            var equation = inputVal;
-            var lastChar = equation[equation.length - 1];
-
-            equation = equation.replace(/x/g, '*');
-
-            if (lastChar == '%') {
-                var operate = new RegExp("[-+*x]");
-                equation = equation.replace(/%/g, '/100');
-                var prefix = '';
-                for (var f = 0; f < inputVal.length; f++) {
-                    if (prefix.match(operate)) {
-                        break;
-                    } else if (inputVal.charAt(f) == '%') {
-                        prefix = '';
-                        break;
-                    }
-                    prefix += inputVal.charAt(f);
-                }
-                equation = equation + '*' + prefix;
-            } else if (lastChar != '%' && inputVal.indexOf('%') > -1) {
-                equation = equation.replace(/%/, '/100*');
-            }
-
-            if (operators.indexOf(lastChar) > -1 || lastChar == '.' || lastChar == '^')
-                equation = equation.replace(/(^)|.$/, '');
-
-            if (equation)
-                input.textContent = calculate(equation);
-
-            if (input.textContent.length > 14) {
-                var n;
-                var divs = document.getElementsByClassName('screen');
-                for (n = 0; n < divs.length; n++) {
-                    if (divs[n].className == 'screen') {
-                        divs[n].textContent = divs[n].textContent.substring(0, 15);
-                    }
-                }
-
-                if (!decimalAdded) {
-                    input.textContent = calculate(equation).toPrecision(14);
-                }
-            }
-
-            decimalAdded = false;
-        }
-
-        else if (operators.indexOf(button) > -1) {
-            var lastChar = inputVal[inputVal.length - 1];
-
-            if (inputVal != '' && operators.indexOf(lastChar) == -1)
-                input.textContent += button;
-
-            else if (inputVal == '' && button == '-')
-                input.textContent += button;
-
-            if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-                input.textContent = inputVal.replace(/.$/, button);
-            }
-
-            if (inputVal != '' && button == 'Xy') {
-                input.textContent = inputVal.replace(/Xy/, '');
-                var supTag = document.createElement('SUP');
-                var expNode = document.createTextNode('^');
-                supTag.setAttribute("id", "powerid");
-                input.appendChild(supTag);
-                supTag.appendChild(expNode);
-            }
-
-            if (inputVal.indexOf('^') > -1) {
-                input.textContent = inputVal.replace(/^$/, '');
-            }
-
-            if (inputVal.indexOf('/') > -1 && button == '%') {
-                input.textContent = inputVal.replace(/%/, '');
-            }
-
-            decimalAdded = false;
-        }
-
-        else if (inputVal == Number.POSITIVE_INFINITY || inputVal == Number.NEGATIVE_INFINITY) {
-            input.textContent = inputVal.replace(/Infinity/ig, '');
-        }
-        else if (inputVal === 'NaN') {
-            input.textContent = inputVal.replace(/NaN/ig, '');
-        }
-
-        else if (button == '.') {
-            if (!decimalAdded && inputVal != '') {
-                input.textContent += button;
-                decimalAdded = true;
-            }
-        }
-
-        else {
-            input.textContent += button;
-        }
-
-        e.preventDefault();
-    }
-}
+  }
+  
+  // Catches all button clicks on the page
+  $(".btn").click(function(event) {
+    $(event.target).blur();
+    processButton(event.target);
+  });
+  
+  $(document).on("click", ".calc-history-eq", function(event) {
+    var tokens = calcHistory[parseInt($(event.target).attr("id").substring(2))].tokens;
+    console.log(parseInt($(event.target).attr("id").substring(2)));
+    console.log(calcHistory);
+    console.log(tokens);
+    tokenList = tokens;
+    displayEquation();
+  });
+  
+});

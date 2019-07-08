8891d6775d9f54309288382dd5b663f02d4be703
corgilucas <lucaschew95@gmail.com>
2019-07-08 21:08:11 +0800

    Add files via upload

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -76,7 +76,7 @@ for (var i = 0; i < keys.length; i++) {
             copy(inputVal, 'text/plain');
             button = '';
         }
-        if (button == 'clear') {
+        if (button == 'c') {
             input.textContent = '';
             decimalAdded = false;
         }
